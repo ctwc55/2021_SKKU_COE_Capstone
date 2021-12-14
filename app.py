859a5fef3ff2ca23a2b1d4681c3585c6428c520a
ctwc55 <ssy990408@gmail.com>
2021-12-14 18:29:16 +0000

    connect to gcb and exec google stt api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 import os
 import subprocess
 import youtube_dl
+import yt
 
 #flask run --host=0.0.0.0 --port=5000
 app = Flask(__name__)
@@ -30,7 +32,23 @@ def yt_to_text():
             ydl.download([link])
 
         old_filename = [file for file in os.listdir('./') if file.endswith('.wav')]
-        os.rename(old_filename[0], 'audiofile.wav')
+        os.rename(old_filename[0], 'sample.wav')
+
+        subprocess.call("ffmpeg -i sample.wav -ar 16000 -ac 1 audiofile.wav", shell=True)
+
+        load_dotenv()
+
+        OBJECT_LOCATION = "/home/ubuntu/Projects/2021_SKKU_COE_Capstone/audiofile.wav"
+        OAUTH2_TOKEN = os.environ.get("OAUTH2_TOKEN")
+        OBJECT_CONTENT_TYPE = "audio/x-wav"
+        BUCKET_NAME = "skku-coe-capstone-2021"
+        OBJECT_NAME = "audiofile.wav"
+        subprocess.call(f"curl -X POST --data-binary @{OBJECT_LOCATION} -H \"Authorization: Bearer {OAUTH2_TOKEN}\" -H \"Content-Type: {OBJECT_CONTENT_TYPE}\" \"https://storage.googleapis.com/upload/storage/v1/b/{BUCKET_NAME}/o?uploadType=media&name={OBJECT_NAME}\"", shell=True)
+
+        yt.transcribe_gcs("gs://skku-coe-capstone-2021/audiofile.wav")
+
+        subprocess.call("rm sample.wav", shell=True)
+        subprocess.call("rm audiofile.wav", shell=True)
 
     return render_template('index.html')
 
