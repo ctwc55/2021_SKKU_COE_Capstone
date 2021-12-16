770a54639f50a62b3e293b75ded2825fa5b1a10b
ctwc55 <ssy990408@gmail.com>
2021-12-16 12:26:48 +0000

    update timestamps

--- yt.py
+++ yt.py
@@ -4,7 +4,7 @@ def transcribe_gcs(gcs_uri):
     """Asynchronously transcribes the audio file specified by the gcs_uri."""
     from google.cloud import speech
 
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/google-cloud-speech-cert/skku-coe-capstone-2021-3333962c72ea.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ.get("GOOGLE_KEY_PATH")
 
     client = speech.SpeechClient()
 
@@ -22,11 +22,29 @@ def transcribe_gcs(gcs_uri):
     print("Waiting for operation to complete...")
     response = operation.result()
 
+    yt_tnt = {}
+
     for result in response.results:
         alternative = result.alternatives[0]
-        print("Transcript: {}".format(result.alternatives[0].transcript))
+
+        transcript = result.alternatives[0].transcript
+        transcripts = [x.strip() for x in transcript.split(".")]
+        del transcripts[-1]
+
+        time_idx = 0
+        for text in transcripts:
+            start_times = alternative.words[time_idx].start_time.total_seconds()
+            yt_tnt[f"{int(start_times//60)}:{int(start_times%60)}"] = text+'.'
+            time_idx += len(text.split())
+            print(f"{int(start_times//60)}:{int(start_times%60)}",text+'.')
+
+
+        print("Transcript: {}".format(transcript))
         print("Confidence: {}".format(result.alternatives[0].confidence))
         print("Start time: {}".format(alternative.words[0].start_time.total_seconds()))
 
-    return response
+        #time_info = alternative.words[0].start_time.total_seconds() 
+        #yt_tnt[f"{int(time_info//60)}:{int(time_info%60)}"] = result.alternatives[0].transcript
+
+    return yt_tnt
 
